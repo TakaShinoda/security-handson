1837a01289e51805a42f13feb504d8e446e9290c
Takahiro <tkhr_621@docomo.ne.jp>
2023-05-07 13:47:24 +0900

    検証用に簡易的なログイン処理実装

---
+++ routes/csrf.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const router = express.Router()
+
+router.use(
+  session({
+    secret: "session",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 60 * 1000 * 5
+    }
+  })   
+)
+
+router.use(express.urlencoded({ extended: true }))
+router.use(cookieParser())
+
+let sessionData = {}
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body
+  if(username !== 'user1' || password !== 'Passw0rd!#') {
+    res.status(403)
+    res.send('ログイン失敗')
+    return
+  }
+
+  sessionData = req.session
+  sessionData.username = username
+  res.redirect('/csrf_test.html')
+})
+
+module.exports = router
